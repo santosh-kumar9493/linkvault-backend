6c52413ec94bd8ba685b41e94658f7ccf56af465
santosh <padavalasantosh@gmail.com>
2026-02-15 00:37:48 +0530

    force https fix

--- src/server.js
+++ src/server.js
@@ -22,13 +22,21 @@ app.use(
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-  res.send("LinkVault API running");
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
+app.set("trust proxy", 1);
+
+app.use((req, res, next) => {
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("LinkVault API running");
+});
 
 app.use("/api/upload", require("./routes/uploadRoutes"));
 app.use("/content", require("./routes/contentRoutes"));
